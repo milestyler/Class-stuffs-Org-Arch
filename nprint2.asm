fa4c056f7acca86a36702434428e32081f5008c6
Tyler Miles <milestyler101@gmail.com>
2017-03-30 10:57:01 -0500

    Add files via upload

---
+++ nprint2.asm
@@ -0,0 +1,22 @@
+global _start
+_start:
+ jmp short ender
+ starter:
+    xor eax, eax
+    xor ebx, ebx
+    xor edx, edx
+    xor ecx, ecx
+    mov al, 0x4
+    mov bl, 0x1
+    pop ecx
+    mov dl, slength
+    int 0x80
+    xor eax, eax
+    mov al, 0x1
+    xor ebx,ebx
+    int 0x80
+ ender:
+    call starter
+ message:
+    db 'Tyler Miles'
+ slength equ $-message
